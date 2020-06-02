50d3dd85190c4beff2ba5e62f65984e644e290c4
oluabe <65191686+oluabe@users.noreply.github.com>
2020-06-02 10:15:57 -0400

    Add files via upload

---
+++ Coursera_Capstone.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#  <h2>About this Notebook</h2>
+
+# <h4>This notebook is to be submitted to GitHub<h4>
+
+# ### Importing Needed packages
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[4]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
